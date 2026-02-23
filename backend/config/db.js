746924cb76b48c2aeaac352a4f2fccbb57649574
johkensut24 <kennethsutter06@gmail.com>
2026-02-23 16:34:22 +0800

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -12,7 +12,7 @@ const pool = new Pool({
   connectionString:
     process.env.DATABASE_URL ||
     `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
-  ssl: isProduction ? { rejectUnauthorized: false } : false,
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
 });
 
 pool.on("connect", () => {
